e886b0070524218f5aee44cb39a21e7d1cdf72bd
Satyam <k98satyam@gmail.com>
2022-07-31 14:20:47 +0530

    max depth of bnary tree

---
+++ maxDepth.java
@@ -0,0 +1,25 @@
+//https://leetcode.com/problems/maximum-depth-of-binary-tree/
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class maxDepth {
+    public int maxDepth(TreeNode root) {
+        if (root == null)
+            return 0;
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    }
+}
